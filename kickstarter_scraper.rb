86ae200c96d37b4eb0f0dc5bfc50d96a994c42d3
boruch d <dubywider@gmail.com>
2019-06-11 23:07:01 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,6 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'open-uri'
+require "pry"
 def create_project_hash
   # write your code here
   html= File.read('fixtures/kickstarter.html')
